a43c1c51e3d2528aa40717493984e7676666f884
sumit13071999 <59279030+sumit13071999@users.noreply.github.com>
2020-03-03 15:13:24 +0530

    Update towerofhonoi.java

--- towerofhonoi.java
+++ towerofhonoi.java
@@ -1,3 +1,4 @@
+/* Question-> implement the tower of honoi problem using recursion*/
 package recursion;
 
 import java.util.Scanner;
@@ -6,12 +7,14 @@ public class towerofhonoi {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
+		// take input 
 		int n=sc.nextInt();
+		//call function
 		TowerofHonoi(n,'A','B','C');
 
 	}
 	public static void TowerofHonoi(int n,char src,char dest,char helper)
-	{
+	{          
 		if(n==0)
 		{
 			return;
